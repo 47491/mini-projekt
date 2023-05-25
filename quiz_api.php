d961378c65839ce6cf5e0dea0757d7656d2c5c00
47491 <47491@gymnasium.ax>
2023-05-25 11:24:50 +0300

    fixed backend
    counter funkar dåligt och slutet correcta svar

--- quiz_api.php
+++ quiz_api.php
@@ -6,77 +6,75 @@ $dbName = "quiz";
 
 $db = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName);
 
-function skrivRandomRad()
-{
-    global $db;
+if (!function_exists('skrivRandomRad')) {
+    function skrivRandomRad()
+    {
+        global $db;
 
-    if (mysqli_connect_errno()) {
-        echo "Anslutningen misslyckades <br>" . mysqli_error($db);
-        exit();
-    }
+        if (mysqli_connect_errno()) {
+            echo "Anslutningen misslyckades <br>" . mysqli_error($db);
+            exit();
+        }
 
-    $prevPictureId = isset($_SESSION['prevPictureId']) ? $_SESSION['prevPictureId'] : null;
+        $prevPictureId = isset($_SESSION['prevPictureId']) ? $_SESSION['prevPictureId'] : null;
 
-    do {
         $result = mysqli_query($db, "SELECT id, svar, bild FROM quiz_fragor WHERE id != '$prevPictureId' ORDER BY RAND() LIMIT 1");
         $row = mysqli_fetch_assoc($result);
         $id = $row['id'];
         $svar = $row['svar'];
         $bild = $row['bild'];
-    } while (!$id);
 
-    $_SESSION['prevPictureId'] = $id;
+        $_SESSION['prevPictureId'] = $id;
 
-    echo "<div id='bild-container'>";
-    echo "<img id='bild' src='data:image/jpeg;base64," . base64_encode($bild) . "' alt='Bild'>";
-    echo "</div>";
+        echo "<div id='bild-container'>";
+        echo "<img id='bild' src='data:image/jpeg;base64," . base64_encode($bild) . "' alt='Bild'>";
+        echo "</div>";
 
-    $alternativ = array($svar);
+        $alternativ = array($svar);
 
-    while (count($alternativ) < 4) {
-        $altResult = mysqli_query($db, "SELECT svar FROM quiz_fragor WHERE svar != '$svar' ORDER BY RAND() LIMIT 1");
-        $altRow = mysqli_fetch_assoc($altResult);
-        $altSvar = $altRow['svar'];
+        while (count($alternativ) < 4) {
+            $altResult = mysqli_query($db, "SELECT svar FROM quiz_fragor WHERE svar != '$svar' ORDER BY RAND() LIMIT 1");
+            $altRow = mysqli_fetch_assoc($altResult);
+            $altSvar = $altRow['svar'];
 
-        if (!in_array($altSvar, $alternativ)) {
-            $alternativ[] = $altSvar;
-        }
-    }
-
-    shuffle($alternativ);
-
-    $correctAnswerId = "";
-
-    foreach ($alternativ as $index => $altSvar) {
-        $altId = "alternativ_" . ($index + 1);
-
-        if ($index == 0) {
-            $correctAnswerId = $altId;
+            if (!in_array($altSvar, $alternativ)) {
+                $alternativ[] = $altSvar;
+            }
         }
 
-        echo "<a class='alternativ' id='$altId' data-correct-answer='$svar'>$altSvar</a>";
+        shuffle($alternativ);
+
+        $correctAnswerId = "";
+
+        foreach ($alternativ as $index => $altSvar) {
+            $altId = "alternativ_" . ($index + 1);
+
+            if ($index == 0) {
+                $correctAnswerId = $altId;
+            }
+
+            echo "<a class='alternativ' id='$altId' data-correct-answer='$svar'>$altSvar</a>";
+        }
+
+        $_SESSION['questionCounter']++;
+        $_SESSION['questionCounter']++;
+
+        if ($_SESSION['questionCounter'] > $_SESSION['totalQuestions']) {
+            $_SESSION['questionCounter'] = $_SESSION['totalQuestions'];
+            $_SESSION['questionCounter'] = $_SESSION['totalQuestions'];
+        }
+
+        mysqli_close($db);
     }
-
-    $_SESSION['questionCounter']++; // Increment the question counter
-    $_SESSION['questionCounter']++; 
-
-    if ($_SESSION['questionCounter'] > $_SESSION['totalQuestions']) {
-        $_SESSION['questionCounter'] = $_SESSION['totalQuestions']; // Limit the question counter to the total number of questions
-        $_SESSION['questionCounter'] = $_SESSION['totalQuestions']; 
-    }
-
-    mysqli_close($db);
 }
 
-
-session_start();
-
 $result = mysqli_query($db, "SELECT COUNT(*) AS total FROM quiz_fragor");
- $_SESSION['totalQuestions'] = $row['total'];
+$row = mysqli_fetch_assoc($result);
+$_SESSION['totalQuestions'] = $row['total'];
 
 if (!isset($_SESSION['questionCounter']) || isset($_POST['answer'])) {
     $_SESSION['questionCounter'] = 1;
-    $_SESSION['wrongAnswers'] = 0; 
+    $_SESSION['wrongAnswers'] = 0;
 } else {
     $_SESSION['questionCounter']++;
 }
